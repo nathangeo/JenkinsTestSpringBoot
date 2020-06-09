4a6ecea9e5959024f79aef6f6d8d2a16d47ff9a7
nathan albin <nathan@radhet.com>
2020-06-09 10:27:25 -0400

    hdfhdh

--- src/main/java/com/sn/controller/UserController.java
+++ src/main/java/com/sn/controller/UserController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -26,6 +27,7 @@ public class UserController {
         return userService.getAllUsers();
     }
 
+    @CrossOrigin(origins = "http://localhost:4200",allowedHeaders = "*")
     @RequestMapping(value = "/adduser", method = RequestMethod.POST,
             consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(code = HttpStatus.OK)
